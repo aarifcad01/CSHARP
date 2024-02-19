628a496814f6da5e048f2b3cf42f199d4d352d94
aarifcad01 <aarifsultan558@gmail.com>
2024-02-19 16:53:42 +0530

    learn c# basics

--- Program.cs
+++ Program.cs
@@ -10,3 +10,25 @@ float b=1.101f;
 Console.WriteLine(b);
 Console.WriteLine("the given value b is " + b);
 
+double c=2.202D;
+Console.WriteLine(c);
+Console.WriteLine("the gives value c is changed to "+ c);
+Console.WriteLine(c.GetType());
+
+
+char s='p';
+Console.WriteLine(s);
+Console.WriteLine("the given value for c is " + s);
+Console.WriteLine(s.GetType());
+
+string harsh="yaru adichu pori kalangi boomi adhirutho odambula teriavanthanda arvindhu";
+Console.WriteLine(harsh);
+Console.WriteLine("the given value for c is " + harsh);
+Console.WriteLine(harsh.GetType());
+
+int getvalue;
+getvalue=int.Parse(console.ReadLine());
+Console.WriteLine(getvalue);
+Console.WriteLine("the given value through console for getvalue is " + getvalue);
+
+
