d61609b99c1a7e7040e14439bf08b6fb63dfa9db
aarifcad01 <aarifsultan558@gmail.com>
2024-02-19 15:12:57 +0530

    new repositary created c#

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+int a=20;
+Console.WriteLine(a);
+Console.WriteLine("the given value for a is " + a);
+
+// int , float , double, string, char, long, boolean, array
+float b=1.101f;
+Console.WriteLine(b);
+Console.WriteLine("the given value b is " + b);
+
