a618fe25de6ecad3fd9bb4c2219b3ba697e30d96
aarifcad01 <aarifsultan558@gmail.com>
2024-02-21 18:02:31 +0530

    added done

---
+++ learn21/Program.cs
@@ -0,0 +1,42 @@
+﻿
+using System;
+
+    class Program
+    {
+        static void Main(String[]args)
+        {//addition
+            int x = 20 + 30;
+            Console.WriteLine(x);
+        //subtraction
+            int a = 15;
+            int b = 20;
+            Console.WriteLine(a - b);
+        //multiplication      
+            int c = 10;
+            int d = 10;
+            Console.WriteLine(c*d);
+        //division
+            int e = 50;
+            int f = 2;
+            Console.WriteLine(e/f);
+             
+        //modulus
+            int g = 10;
+            int h = 30;
+            Console.WriteLine(g % h);
+        
+        //increment
+            int i= 10;
+            int j= 10;
+            Console.WriteLine(i++j);
+        
+        //decrement
+            int k= 200;
+            int l= 300;
+            Console.WriteLine(k--l);
+
+        }
+    }
+
+
+
