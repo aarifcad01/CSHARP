c0f834d81aaccb5b138df23b4988e500d0c366b0
aarifcad01 <aarifsultan558@gmail.com>
2024-02-22 17:59:58 +0530

    modified done

---
+++ learn22/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace aarif{ 
+class Program{
+    static void Main(string[] args){
+
+         Kalaingar kr=new Kalaingar();
+         Console.WriteLine("Hi I am Dr. Muthuvel Karunanithi (Kalaingar)");
+         Console.WriteLine("I have "+kr.getHouse());
+         Console.WriteLine("I have "+kr.getNewspaper());
+         Console.WriteLine("I have "+kr.getpoliticalpartyname());
+         Console.WriteLine("I have "+kr.getProductioncompany());
+    }
+}
+}
